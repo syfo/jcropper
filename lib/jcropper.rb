3f58f82c6d237c6b7864ac1e3e38835f89b7b802
Ryan Ziegler <ryan@symbolforce.com>
2011-05-09 06:17:33 -0400

    no longer using unnecessary constant ROOT, changing method of loading processor

--- lib/jcropper.rb
+++ lib/jcropper.rb
@@ -1,6 +1,8 @@
 require 'railtie' if defined?(Rails) and Rails.version.split('.').first.to_i > 2
-ROOT = File.join(File.dirname(__FILE__), 'jcropper')
-require File.join(ROOT, 'jcropper')
-require File.join(ROOT, 'helpers')
-require File.join(ROOT, 'cropped_image')
-Paperclip.autoload 'Jcropper', File.join(ROOT, '../paperclip_processors/jcropper.rb')
+root = File.join(File.dirname(__FILE__), 'jcropper')
+require File.join(root, 'jcropper')
+require File.join(root, 'helpers')
+require File.join(root, 'cropped_image')
+
+#Paperclip.autoload 'Jcropper', File.join(root, '../paperclip_processors/jcropper.rb')
+require File.join(root, '../paperclip_processors/jcropper.rb')
