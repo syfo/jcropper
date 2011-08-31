5fc85c0fa99808c4165f9068f37a7ed076929a82
Ryan Ziegler <ryan@symbolforce.com>
2011-08-31 16:52:19 -0400

    fixing tasks.rb so you can load tasks via require 'jcropper/tasks'

--- lib/jcropper/tasks.rb
+++ lib/jcropper/tasks.rb
@@ -1,1 +1,1 @@
-load File.join(File.dirname(__FILE__), '..', '..', 'tasks', 'jcropper.rake') if defined? Rake
+load File.join(File.dirname(__FILE__), '..', 'tasks', 'jcropper.rake') if defined? Rake
